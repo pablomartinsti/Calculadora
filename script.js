bfaaf5292ee2bf0b3218001132a177a781552714
pablomartinsti <pablomartinsti@outlook.com>
2024-06-19 22:47:12 -0300

    commit

---
+++ script.js
@@ -0,0 +1,61 @@
+function adicionarCaracter(number) {
+
+    verificaçaoError(number)
+
+    if (document.querySelector(".display").value.length === 0 && !Number(number)) {
+        return
+    }
+
+    const display = document.querySelector(".display").value
+
+    document.querySelector(".display").value = display + number
+
+}
+
+function limpaTela() {
+
+    document.querySelector(".display").value = ""
+
+}
+
+function calcular() {
+
+    const display = document.querySelector(".display").value
+
+    document.querySelector(".display").value = eval(display)
+
+
+}
+
+function inverterSinal() {
+    const display = document.querySelector(".display").value
+
+    if (Number(document.querySelector(".display").value)) {
+
+        document.querySelector(".display").value = display * -1
+
+    }
+
+}
+function deletarUltimaLetra() {
+
+    document.querySelector(".display").value
+        = document.querySelector(".display").value.slice(0, -1)
+
+}
+
+function verificaçaoError(numberDigit){
+
+
+    let ultimoValorNoInputResultado = document.querySelector(".display").value 
+    [document.querySelector(".display").value.length -1]
+
+    if(
+        ultimoValorNoInputResultado && !Number(ultimoValorNoInputResultado)&&
+        !Number(numberDigit) &&
+        ultimoValorNoInputResultado != 0 &&
+        numberDigit != 0 
+    ){
+        deletarUltimaLetra();
+    }
+}
